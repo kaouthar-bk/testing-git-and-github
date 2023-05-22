830b46ef2680f0489b88f5af588f8ec58631f46d
ibtissamHd <hdidouchibtissam@gmail.com>
2023-05-22 15:03:00 +0100

    hi

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-    console.log("Hello, world");
+    console.log("Hello, world! are you ready?");
 });
 app.post("/", (req, res) => {
     console.log("Post request");
